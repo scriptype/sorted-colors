2677d131d449482880edefb34578ba066a143434
scriptype <scriptyper@gmail.com>
2020-06-04 23:49:25 +0300

    Add more tests for Model.setColorsData

--- src/app/model.spec.js
+++ src/app/model.spec.js
@@ -9,11 +9,26 @@ test('Model.setColorsData', async t => {
     return thing.type && thing.name && thing.hsl && thing.rgb && thing.hex
   }
 
+  const hasDifferentName = (thing, things) => {
+    return !things.find(otherThing => otherThing.name === thing.name)
+  }
+
   const storage = {}
 
   Model.setColorsData('colorsTable', storage)
 
-  t.is(storage.colorsData.length, 139, 'Correct number of items are retrieved to storage')
+  t.is(storage.colorsData.length, 139,
+    'Correct number of items are retrieved')
 
-  t.true(storage.colorsData.every(isColor), 'Retrieved items are really colors')
+  t.true(storage.colorsData.every(isColor),
+    'Retrieved items are really colors')
+
+  storage.colorsData.every(color => {
+    const otherColors = storage.colorsData.filter(c => c.hex !== color.hex)
+    t.is(otherColors.length, storage.colorsData.length - 1,
+      '# of colors with a different hex is exactly: AllColors - 1')
+
+    t.true(hasDifferentName(color, otherColors),
+      'Every color has a different name')
+  })
 })
